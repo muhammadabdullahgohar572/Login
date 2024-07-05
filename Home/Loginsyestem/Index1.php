6a825ba9a5f29ffd3f6718bab2d98e0695ceced7
Abdullah <muhammadabdullahgohar572@gmail.com>
2024-07-05 20:15:39 +0500

    first commit

--- Home/Loginsyestem/Index1.php
+++ Home/Loginsyestem/Index1.php
@@ -1,14 +1,28 @@
+<?php
+
+
+session_start();
+
+if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] != true) {
+
+    header("location: login.php");
+    exit;
+}
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Welcome <?php echo $_SESSION['username'] ?>< /title>
 </head>
 
 <body>
     <?php require "./Navbar.php" ?>
+    <h1>Welcome <?php echo $_SESSION['username'] ?></h1>
     <!-- 
     git remote add origin https://github.com/muhammadabdullahgohar572/Login.git
     git branch -M main
